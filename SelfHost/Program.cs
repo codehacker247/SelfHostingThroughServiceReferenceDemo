8b28de150264213f138bd0abb610485a6a60aa7a
Rohit <rohitkumarmandal34@gmail.com>
2023-01-24 19:23:54 +0530

    Add project files.

---
+++ SelfHost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathServiceLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace SelfHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var servicehost = new ServiceHost(typeof(MathService)))
+            {
+                servicehost.Open();
+
+                Console.WriteLine("Your WCF Service is running");
+                Console.WriteLine("Your WCF Math Service is running is listening on below endpoints");
+                foreach(ServiceEndpoint endpoint in servicehost.Description.Endpoints)
+                {
+                    Console.WriteLine("Address : {0} Binding Name : {1}",endpoint.Address.ToString(),endpoint.Binding.Name);
+                }
+                Console.WriteLine("Press any key to stop your WCF Math Service.");
+                Console.ReadKey();  
+
+                servicehost.Close();    
+            }
+
+        }
+    }
+}
---
+++ WindowsClient1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsClient1.MathServiceReference;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+namespace WindowsClient1
+{
+    public partial class Form1 : Form
+    {
+        private MathServiceClient proxy = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+           proxy = new MathServiceClient("MathService_netTcp");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyNumbers obj = new MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);    
+            textBox3.Text= proxy.Add(obj).ToString(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MyNumbers obj = new MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = proxy.Subtract(obj).ToString();
+        }
+    }
+}
